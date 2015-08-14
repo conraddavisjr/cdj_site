02ee4cab3dfc3b2f6f2de5a763b796237f02f07a
Conrad Davis Jr <cdavis@agencypja.com>
2015-08-13 22:23:00 -0400

    center intro Title

--- js/intro.js
+++ js/intro.js
@@ -6,14 +6,29 @@ jQuery(function($){
 		},
 		setupElements: function() {
 			//vars
-
+			var windowHeight = window;
 			//elements
-			this.$introTitle = $('#site-intro h1');
+			this.introTitle = $('#site-intro h1');
 		},
 		
 		introAnimation: function() {
-			//grab the title dimensions
-			intro.$introTitle
+			//grab dimensions for the title
+			var titleHeight = intro.introTitle.height();
+			var titleWidth = intro.introTitle.width();
+			var windowHeight = $(window).innerHeight();
+			
+			//position the introTitle in the center of the page
+			//calc the top px.
+			var topPos = (windowHeight / 2) - (titleHeight / 2);
+			//calc the left px.
+			var leftPos = -(titleWidth / 2);
+			
+			intro.introTitle.css({
+				'display' : 'block',
+				'top'  : topPos,
+				'left' :  '50%',
+				'marginLeft' : leftPos
+			});
 		},
 	};
 
