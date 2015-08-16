80b929e6fe040d1ae0b94613ff08174ae978c442
Conrad Davis Jr <cdjcorporation@gmail.com>
2015-08-16 15:33:14 -0700

    continued build for About Pg

--- js/intro.js
+++ js/intro.js
@@ -184,7 +184,7 @@ jQuery(function($){
 				intro.aboutPage.show();
 				intro.pageMarquee.css({backgroundImage : 'url(img/about-hover-img.jpg)'});
 				//slide the About Marquee into position
-				TweenLite.to(intro.pageMarquee, 1, {height: '26%', opacity: 1});    
+				TweenLite.to(intro.pageMarquee, 1, {height: '200px', opacity: 1});    
 				//slide the About page into frame
 				//intro.aboutPage.css({'paddingTop' : intro.pageMarquee.height()});
 				TweenLite.to(intro.aboutPage, 1, {position:'fixed', top: intro.header.height()});
