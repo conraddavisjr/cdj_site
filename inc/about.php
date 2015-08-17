593973438b063a609e1efaad33dea4b0a21180be
Conrad Davis Jr <cdjcorporation@gmail.com>
2015-08-17 16:10:05 -0700

    Integrating Contact Form

---
+++ inc/about.php
@@ -0,0 +1,348 @@
+<!---About Page -->
+	<section id="about" class="section">
+		<div class="container">
+			<h2>"Be like water"</h2>
+			<p class="about-subtitle">I am adaptive and ever-changing. I am a glutten for new challenges. I prefer to live life at the edge of my comfort.</p>
+			<summary>
+				<h4>Fulltime: Web Developer | PJA Advertizing + Marketing</h4>
+				<p>Develop websites, web applications, CMSs, and Rich Media Ads using the latest technologies and techniques including HTML5, CSS3, jQuery, AJAX, AS3, responsive design, standards-based markup, 3rd party API integration, and more.</p>
+				<h4>Partime: Freelance Developer | Designer | UX</h4>
+				<p>Work closely with clients, to help achieve their objectives using the web, video, and print. Work through the full life of the project through all aspects of its development including strategy, accounts, project management, UX, design, development, social media, analytics, and user testing.</p>
+				<h4>Newest Endeavour: Native App Development (Android).</h4>
+				<br>
+				<h4>Current infatuation: The mobile world and the Internet of things... and coffee.</h4>
+				
+			</summary>
+			<ul>
+				<li>
+					<a href="#0" data-type="member-1">
+						<figure>
+							<img src="img/about-bio-bucket.jpg" alt="Bio image Icon">
+							<div class="img-overlay"><span>View Bio</span></div>
+						</figure>
+
+						<div class="member-info">
+							A little bit about Me <span>Bio</span>
+						</div> <!-- member-info -->
+					</a>
+				</li>
+				<li>
+					<a href="#0" data-type="member-2">
+						<figure>
+							<img src="img/about-work-bucket.jpg" alt="Work image Icon">
+							<div class="img-overlay"><span>View Skills</span></div>
+						</figure>
+
+						<div class="member-info">
+							My Toolbox <span>See the skills</span>
+						</div> <!-- member-info -->
+					</a>
+				</li>
+			</ul>
+		</div> <!-- container -->
+	</section> <!---About Page -->
+
+	<div class="overlay"></div>
+</main>
+
+<div class="member-bio member-1">
+	<div class="member-bio-pict">
+		<img src="img/about-profile.jpg" alt="Bio image">
+	</div> <!-- member-bio-pict -->
+
+	<div class="bio-content">
+		<h2>Presentation is life</h2>
+		<p>Since I was six years old, I've been around computers and gadgets. I was the child who meddled with devices and toys; 
+        piecing them apart and seeing what they were made of. I wanted to know what made things work, why it was attractive to me,
+        and how I could improve it. I was naturally drawn to fine arts, performance, and presentation. As a child I drew, sang, 
+        played piano, designed presentations on my PC and made video games for my younger brother and I. I enjoyed impressing people
+        with beautiful art.</p>                
+        
+        <p>I love presentation, and websites are among the epitome of presentations. When I build websites,
+        I don't think about the site, I think about the people I'm building it for. Websites are, as I like to think of them, human 
+        interaction and I apply the interactions from my life to my work. Let's take design into consideration. When you see something 
+        you are attracted to for the first time, you're not immediately evaluating its traits, initially, you are simply indulged 
+        in the attraction; it's intuitive and it's natural; that is beauty. I strive to incorporate these qualities and invoke these 
+        emotions into my design.</p>
+
+        <p>Development is the "electricity" of our industry, its everywhere and involved in practically everything we do. It runs 24/7 behind the curtains and works until the moment it doesn't; our lights turn out, and everything comes to a hault while we suddenly realize how many things it was powering. Solid development works without you noticing. It's rapid, problem-free, and graceful; those are the qualities I focus on when I develop. </p>
+         
+        <p>Great design draws us in, it makes things useable; great development keeps us there, it makes things
+        useful. The marriage of the two creates effective technology and I absolutely love when they come together.</p>
+
+        <p>I love learning new things, this passion led me to develop quite a few hobbies and interests like dancing, playing piano, exercise, singing (shower quality), and trying something new quite often. </p>
+	</div> <!-- bio-content -->
+</div> <!-- member-bio -->
+
+<div class="member-bio member-2">
+	<div class="member-bio-pict">
+		<img src="img/skills-profile.jpg" alt="Member Bio image">
+	</div> <!-- member-bio-pict -->
+
+	<div class="bio-content">
+		<h2>Geek + Chic = Sleek</h2>
+		<p style="text-align:center;">My love for learning has earned me an expansive repertoire of proficiencies and I continue to improve it. <br>
+		Whether I'm on a PC or a MAC, I feel right at home. Please, take a look.</p>
+		<h3>SOFTWARE</h3>      
+        <div class="skillIcons">
+            <figure>
+                    <img src="img/icons/DW.png" alt="Dreamweaver" />
+                    <figcaption>Dreamweaver</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/aptana.jpg" alt="Aptana" />
+                    <figcaption>Aptana</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/coda.png" alt="Coda" />
+                    <figcaption>Coda</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/sublimetext3.png" alt="Sublime Text" />
+                    <figcaption>Sublime Text</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/codekit.png" alt="Codekit" />
+                    <figcaption>Codekit</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/PhpMyAdmin.png" alt="PhpMyAdmin" />
+                    <figcaption>PhpMyAdmin</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/sequelpro.png" alt="Sequel Pro" />
+                    <figcaption>Sequel Pro</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/virtualhostx.png" alt="VirtualHostX" />
+                    <figcaption>VirtualHostX</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/wamp.png" alt="WAMP" />
+                    <figcaption>WAMP</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/mamp.png" alt="MAMP" />
+                    <figcaption>MAMP</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/terminal.png" alt="Terminal" />
+                    <figcaption>Terminal</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/putty.png" alt="Putty" />
+                    <figcaption>Putty</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/transmit.png" alt="Transmit" />
+                    <figcaption>Transmit</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/filezilla.png" alt="Filezilla" />
+                    <figcaption>Filezilla</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/git.png" alt="Git" />
+                    <figcaption>Git</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/beanstalk.png" alt="Beanstalk" />
+                    <figcaption>Beanstalk</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/bitbucket.png" alt="Bitbucket" />
+                    <figcaption>Bitbucket</figcaption>
+            </figure>
+             <figure>
+                    <img src="img/icons/github.png" alt="GitHub" />
+                    <figcaption>GitHub</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/tower.png" alt="Tower" />
+                    <figcaption>Tower</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/sourcetree.png" alt="SourceTree" />
+                    <figcaption>SourceTree</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/googletagmanager.png" alt="Google Tag Manager" />
+                    <figcaption>Google Tag Manager</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/vwo.png" alt="Visual Website optimizer" />
+                    <figcaption>Visual Website optimizer</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/kaleidoscope.png" alt="Kaleidoscope" />
+                    <figcaption>Kaleidoscope</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/textwrangler.png" alt="Text wrangler" />
+                    <figcaption>Text wrangler</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/indesign.png" alt="Indesign" />
+                    <figcaption>Indesign</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/PS.png" alt="Photoshop" />
+                    <figcaption>Photoshop</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/AI.png" alt="Illustrator" />
+                    <figcaption>Illustrator</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/flash.png" alt="Flash" />
+                    <figcaption>Flash</figcaption>
+            </figure>   
+            <figure>
+                    <img src="img/icons/docs.png" alt="Google Docs" />
+                    <figcaption>Google Docs</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/word.png" alt="Word" />
+                    <figcaption>Word</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/pages.png" alt="Pages" />
+                    <figcaption>Pages</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/spreadsheet.png" alt="Spreadsheet" />
+                    <figcaption>Spreadsheet</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/excel.png" alt="Excel" />
+                    <figcaption>Excel</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/numbers.png" alt="Numbers" />
+                    <figcaption>Numbers</figcaption>
+            </figure>   
+            <figure>
+                    <img src="img/icons/PP.png" alt="Power Point" />
+                    <figcaption>Power Point</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/proTools.png" alt="Pro Tools LE" />
+                    <figcaption>Pro Tools LE</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/keynote.png" alt="Keynote" />
+                    <figcaption>Keynote</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/browserstack.png" alt="Browserstack" />
+                    <figcaption>Browserstack</figcaption>
+            </figure>      
+            <figure>
+                    <img src="img/icons/crossbrowsertesting.png" alt="Crossbrowsertesting.com" />
+                    <figcaption>Cross browser testing.com</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/saucelabs.png" alt="Sauce Labs" />
+                    <figcaption>Sauce Labs</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/Parallels.png" alt="Parallels" />
+                    <figcaption>Parallels</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/basecamp.png" alt="BaseCamp" />
+                    <figcaption>BaseCamp</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/ac.png" alt="activeCollab" />
+                    <figcaption>activeCollab</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/goToMeeting.png" alt="GoTo Meeting" />
+                    <figcaption>GoTo Meeting</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/glip.png" alt="Glip" />
+                    <figcaption>Glip</figcaption>
+            </figure>
+        </div> <!--skillIcons-->
+        <h3>CODING/ PROGRAMMING</h3>
+        <div class="skillIcons">
+            <figure>
+                    <img src="img/icons/php.png" alt="PHP" />
+                    <figcaption>PHP</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/mysql.png" alt="Mysql" />
+                    <figcaption>Mysql</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/linux.png" alt="Linux" />
+                    <figcaption>Linux</figcaption>
+            </figure>         
+            <figure>
+                    <img src="img/icons/css3.png" alt="CSS3" />
+                    <figcaption>CSS3</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/sass.png" alt="SASS" />
+                    <figcaption>SASS</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/javascript.png" alt="Javascript" />
+                    <figcaption>Javascript</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/jquery.png" alt="jQuery" />
+                    <figcaption>jQuery</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/greensock.png" alt="GreenSock" />
+                    <figcaption>GreenSock</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/nodejs.png" alt="Node.js" />
+                    <figcaption>Node.js</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/html5.png" alt="HTML5" />
+                    <figcaption>HTML5</figcaption>
+            </figure>  
+            <figure>
+                    <img src="img/icons/as3.png" alt="ActionScript3" />
+                    <figcaption>ActionScript3</figcaption>
+            </figure>      
+        </div>
+        <h3>ADDITIONAL</h3>
+        <div class="skillIcons">
+            <figure>
+                    <img src="img/icons/drupal.png" alt="Drupal7" />
+                    <figcaption>Drupal7</figcaption>
+            </figure>  
+            <figure>
+                    <img src="img/icons/processwire.png" alt="Processwire" />
+                    <figcaption>Processwire</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/cms.png" alt="Custom CMS" />
+                    <figcaption>Custom CMS</figcaption>
+            </figure> 
+            <figure>
+                    <img src="img/icons/bootStrap.png" alt="BootStrap" />
+                    <figcaption>BootStrap</figcaption>
+            </figure>  
+            <figure>
+                    <img src="img/icons/jqueryMobile.png" alt="jQuery Mobile" />
+                    <figcaption>jQuery Mobile</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/responsive.png" alt="Responsive" />
+                    <figcaption>Responsive</figcaption>
+            </figure>
+            <figure>
+                    <img src="img/icons/grunt.png" alt="Grunt" />
+                    <figcaption>Grunt</figcaption>
+            </figure>
+        </div>
+	</div> <!-- bio-content -->
+</div> <!-- member-bio --><!--About -->
