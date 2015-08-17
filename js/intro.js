f3a77322ae3194420666cc3ef860c2831be38b19
Conrad Davis Jr <cdjcorporation@gmail.com>
2015-08-17 03:42:51 -0700

    Home btn linking w. func and animation

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,5 @@
 jQuery(function($){
-	var intro = {
+	var main = {
 		init: function() {
 			this.setupElements();
 			this.introAnimation();
@@ -21,49 +21,51 @@ jQuery(function($){
 			this.pageMarquee = $('.page-marquee');
 			this.mainContainer = $('main, .member-bio');
 			this.aboutPage = $('#about');
+			this.homeLink = $('.siteTitle a');
+			
 		},
 		
 		introAnimation: function() {
 			//dimensions for the intro line
-			var introLineHeight = intro.introLine.height();
+			var introLineHeight = main.introLine.height();
 
 			//dimensions for the Intro title
-			var titleHeight = intro.introTitle.height();
-			var titleWidth = intro.introTitle.width() + (intro.introTitle.width() * 0.37);
+			var titleHeight = main.introTitle.height();
+			var titleWidth = main.introTitle.width() + (main.introTitle.width() * 0.37);
 			var windowHeight = $(window).innerHeight();
 
 			//dimensions for the Intro subTitle
-			var subTitleHeight = intro.subTitle.height();
-			var subTitleWidth = intro.subTitle.width();
+			var subTitleHeight = main.subTitle.height();
+			var subTitleWidth = main.subTitle.width();
 
 			//dimensions for the Intro title Cover
-			var introTitleCoverHeight = intro.introTitleCover.height();
+			var introTitleCoverHeight = main.introTitleCover.height();
 
 			//dimensions for the Intro subTitle Cover
-			var introSubTitleCoverHeight = intro.introSubTitleCover.height();
+			var introSubTitleCoverHeight = main.introSubTitleCover.height();
 			console.log("introSubTitleCoverHeight" + introSubTitleCoverHeight);
 
 			//dimensions for the nav menu
-			var navHeight = intro.nav.height();
+			var navHeight = main.nav.height();
 
 			
 			//position the introLine in the center of the page
 			//calc the top px.
 			var lineTopPos = (windowHeight / 2);
-			intro.introLine.css({
+			main.introLine.css({
 				'display' : 'block',
 				'top'  : lineTopPos
 			});
 			//grab the intro line's offset
-			var introLineOffset = intro.introLine.offset();
+			var introLineOffset = main.introLine.offset();
 			
 			//position the title covers
 			//introTitle
-			intro.introTitleCover.css({
+			main.introTitleCover.css({
 				'marginTop' : (windowHeight / 2)
 			})
 			//introSubTitle
-			intro.introSubTitleCover.css({
+			main.introSubTitleCover.css({
 				'marginTop' :(windowHeight / 2) - (introSubTitleCoverHeight)
 			})
 
@@ -72,7 +74,7 @@ jQuery(function($){
 			var titleLeftPos = -(titleWidth / 2);
 			//calc the top px.
 			var titleTopPos = introLineOffset.top - (titleHeight / 2);
-			intro.introTitle.css({
+			main.introTitle.css({
 				'position' : 'absolute',
 				'opacity' : '0',
 				'transform' : 'scale(2)',
@@ -88,7 +90,7 @@ jQuery(function($){
 			//calc the left px.
 			var subTitleLeftPos = -(subTitleWidth / 2);
 			console.log("subTitleLeftPos: " + subTitleLeftPos);
-			intro.subTitle.css({
+			main.subTitle.css({
 				'display' : 'inline-block',
 				'top'  : subTitleTopPos,
 				'left' :  '50%',
@@ -96,13 +98,13 @@ jQuery(function($){
 			});
 
 			//position the nav underneath the page fold
-			intro.nav.css({
+			main.nav.css({
 				bottom: -navHeight
 			});
 
 			//position the nav-hover-overlay above the nav
-			intro.navHoverOverlay.css({
-				//bottom: intro.nav.height()
+			main.navHoverOverlay.css({
+				//bottom: main.nav.height()
 			})
 
 
@@ -113,35 +115,36 @@ jQuery(function($){
 
 			tl = new TimelineLite();
 			//animate the line in
-			tl.to(intro.introLine, 1, {width: '100%'})	
+			tl.to(main.introLine, 1, {width: '100%'})	
 			//animate the title in
-			.to(intro.introTitle, 1, {top: titleTopPos - (titleHeight + (titleHeight * 0.2)), opacity: 1}, "-=0.3")
+			.to(main.introTitle, 1, {top: titleTopPos - (titleHeight + (titleHeight * 0.2)), opacity: 1}, "-=0.3")
 			//remove the introTitleCover
-			.to(intro.introTitleCover, 0, {display: 'none'}, "-=0.3")
+			.to(main.introTitleCover, 0, {display: 'none'}, "-=0.3")
 			//animate the suibtle in
-			.to(intro.subTitle, 0.7, {top: subTitleTopPos + (subTitleHeight + (subTitleHeight * 0.5)), opacity: 1}, "-=0.5")
+			.to(main.subTitle, 0.7, {top: subTitleTopPos + (subTitleHeight + (subTitleHeight * 0.5)), opacity: 1}, "-=0.5")
 
 			//animate the line and the subTitle out
-			.to(intro.introLine, 1, {left: '100%', width:'0%'}, "+=1")
-			.to(intro.subTitle, 0.7, {top: subTitleTopPos - subTitleHeight}, "-=1")
+			.to(main.introLine, 1, {left: '100%', width:'0%'}, "+=1")
+			.to(main.subTitle, 0.7, {top: subTitleTopPos - subTitleHeight}, "-=1")
 			//move the Title up to the header position
-			.to(intro.introTitle, 1, {top: '15px', scale:1}, "-=1")
+			.to(main.introTitle, 1, {top: '15px', scale:1}, "-=1")
 			//reveal the header and remove the animated title
 			.to($('header'), 0, {position: 'fixed', width: '100%', display: 'block'})
-			.to(intro.introTitle, 0, {display: 'none'})
-			.to(intro.subTitle, 0, {display: 'none'}, "-=0.7")
-			.to(intro.introTitleCover, 0, {display: 'none'}, "-=0.5")
-			.to(intro.introSubTitleCover, 0, {display: 'none'}, "-=0.7")
+			.to(main.introTitle, 0, {display: 'none'})
+			.to(main.subTitle, 0, {display: 'none'}, "-=0.7")
+			.to(main.introTitleCover, 0, {display: 'none'}, "-=0.5")
+			.to(main.introSubTitleCover, 0, {display: 'none'}, "-=0.7")
 			.to($('.header-spacer'), 0, {display:'block'})
 			.to($('.header-spacer'), 0, {display:'block'})
-			.to($('.intro-veil'), 1, {height: intro.header.height() + 30}, "-=1")
+			.to($('.intro-veil'), 1, {height: main.header.height() + 30}, "-=1")
 			//animate the Navigation into frame
 			.to($('nav'), 1, {bottom:0, opacity:1})
 		},
 
 		eventHandlers: function() {
-			intro.navItem.hover(this.navHoverInOverlayHandler, this.navHoverOutOverlayHandler);
-			intro.navItem.on('click', this.callPageHandler);
+			main.navItem.hover(this.navHoverInOverlayHandler, this.navHoverOutOverlayHandler);
+			main.navItem.on('click', this.callPageHandler);
+			main.homeLink
 		},
 
 
@@ -150,30 +153,30 @@ jQuery(function($){
 			//check which item was hovered over
 			switch(this.text){
 				case 'ABOUT' :
-				intro.navHoverOverlay.css({backgroundImage : 'url(img/about-hover-img.jpg)'});
-				intro.navHoverOverlay.attr('class', 'nav-hover-overlay about-btn');
+				main.navHoverOverlay.css({backgroundImage : 'url(img/about-hover-img.jpg)'});
+				main.navHoverOverlay.attr('class', 'nav-hover-overlay about-btn');
 				break;
 				case 'WORK' :
-				intro.navHoverOverlay.css({backgroundImage : 'url(img/work-hover-img.jpg)'});
-				intro.navHoverOverlay.attr('class', 'nav-hover-overlay work-btn');
+				main.navHoverOverlay.css({backgroundImage : 'url(img/work-hover-img.jpg)'});
+				main.navHoverOverlay.attr('class', 'nav-hover-overlay work-btn');
 				break;
 				case 'CONTACT' :
-				intro.navHoverOverlay.css({backgroundImage : 'url(img/contact-hover-img.jpg)'});
-				intro.navHoverOverlay.attr('class', 'nav-hover-overlay contact-btn');
+				main.navHoverOverlay.css({backgroundImage : 'url(img/contact-hover-img.jpg)'});
+				main.navHoverOverlay.attr('class', 'nav-hover-overlay contact-btn');
 				break;
 				default:
 				//do nothing
 			}
 			//reveal the overlay and scale it down
-			TweenLite.to(intro.navHoverOverlay, 0.5, { opacity : 0.6, scale : 1});
+			TweenLite.to(main.navHoverOverlay, 0.5, { opacity : 0.6, scale : 1});
 			//hide the main container
-			intro.mainContainer.css({'zIndex' : 2});
+			main.mainContainer.css({'zIndex' : 2});
 		},
 		navHoverOutOverlayHandler : function(){
 			//hide the overlay
-			TweenLite.to(intro.navHoverOverlay, 0.2, {opacity : 0, scale : 1.3});
+			TweenLite.to(main.navHoverOverlay, 0.2, {opacity : 0, scale : 1.3});
 			setTimeout(function(){
-				intro.mainContainer.css({'zIndex' : 101});
+				main.mainContainer.css({'zIndex' : 101});
 			},200);
 			
 		},
@@ -181,33 +184,33 @@ jQuery(function($){
 			//check which item was clicked
 			switch(this.text){
 				case 'ABOUT' :
-				intro.aboutPage.show();
-				intro.pageMarquee.css({backgroundImage : 'url(img/about-hover-img.jpg)'});
+				main.aboutPage.show();
+				main.pageMarquee.css({backgroundImage : 'url(img/about-hover-img.jpg)'});
 				//slide the About Marquee into position
-				TweenLite.to(intro.pageMarquee, 1, {height: '220px', opacity: 1});    
+				TweenLite.to(main.pageMarquee, 1, {height: '220px', opacity: 1});    
 				//slide the About page into frame
-				//intro.aboutPage.css({'paddingTop' : intro.pageMarquee.height()});
-				TweenLite.to(intro.aboutPage, 1, {position:'fixed', top: intro.header.height()});
+				//main.aboutPage.css({'paddingTop' : main.pageMarquee.height()});
+				TweenLite.to(main.aboutPage, 1, {position:'fixed', top: main.header.height()});
 				break;
 				case 'WORK' :
-				intro.navHoverOverlay.css({backgroundImage : 'url(img/work-hover-img.jpg)'});
-				intro.navHoverOverlay.attr('class', 'nav-hover-overlay work-btn');
+				main.navHoverOverlay.css({backgroundImage : 'url(img/work-hover-img.jpg)'});
+				main.navHoverOverlay.attr('class', 'nav-hover-overlay work-btn');
 				break;
 				case 'CONTACT' :
-				intro.navHoverOverlay.css({backgroundImage : 'url(img/contact-hover-img.jpg)'});
-				intro.navHoverOverlay.attr('class', 'nav-hover-overlay contact-btn');
+				main.navHoverOverlay.css({backgroundImage : 'url(img/contact-hover-img.jpg)'});
+				main.navHoverOverlay.attr('class', 'nav-hover-overlay contact-btn');
 				break;
 				default:
 				//do nothing
 			}
 			//reveal the main container
-			intro.mainContainer.css({'zIndex' : 101});
-			TweenLite.to(intro.navHoverOverlay, 0.3, {opacity : 0, scale : 1.3});
+			main.mainContainer.css({'zIndex' : 101});
+			TweenLite.to(main.navHoverOverlay, 0.3, {opacity : 0, scale : 1.3});
 		}
 
 		
 	};
 
-	intro.init();
-	window.intro = intro;
+	main.init();
+	window.main = main;
 });
